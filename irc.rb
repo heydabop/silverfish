ab247d26598949ce3e52e983f1e46e4bf7d61b5f
heydabop <heydabop@gmail.com>
2013-07-03 18:46:43 -0500

    Initial commit.

---
+++ irc.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/ruby
+
+require 'socket'
+
+#join channels
+def join (socket, channels)
+  for channel in channels
+    puts "SEND: JOIN ##{channel}"
+    socket.puts "JOIN ##{channel}"
+  end
+end
+
+def identify (socket, passwd)
+  puts "SEND: PRIVMSG NickServ :IDENTIFY #{passwd}"
+  socket.puts "PRIVMSG NickServ :IDENTIFY #{passwd}"
+end
+
+#pong server
+def pong (socket, server)
+  puts "SEND: PONG #{server}"
+  socket.puts "PONG #{server}"
+end
+
+channels = ["minecraft"]
+
+irc = TCPSocket.new 'irc.tamu.edu', 6667
+
+puts "SEND: NICK silvrfish"
+irc.puts "NICK silvrfish"
+puts "SEND: USER silvrfish * * :silvrfish"
+irc.puts "USER silvrfish * * :silvrfish"
+
+#wait for auth to finish, join channels and identify upon server sending 001
+while line = irc.gets
+  puts line
+  if line.include? "001"
+    identify irc, "ichaizeu"
+    join irc, channels
+    break
+  end
+end
+
+#puts output
+out = Thread.new{
+  while line = irc.gets
+    puts line
+    #listen for a respond to pings
+    if line.include? "PING :"
+      server = line.split(':')[1]
+      pong irc, server
+      next
+    end
+    #listen for commands, regex to watch for line beginning with &
+    unless %r{PRIVMSG #[[:alnum:]]+ :&}.match(line) == nil
+      index = line.index('&') + 1
+      command = line[index..line.length].split(' ')[0]
+      if command == "botsnack"
+        puts "SEND: PRIVMSG #minecraft :*rrrgghh*"
+        irc.puts "PRIVMSG #minecraft :*rrrgghh*"
+      end
+    end
+  end
+}
+
+out.join
