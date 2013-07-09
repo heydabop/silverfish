75e6c36fc68166fa968af8ba41472d677956079f
heydabop <heydabop@gmail.com>
2013-07-09 13:44:51 -0500

    Added timestamp to console output via tsputs function

--- irc.rb
+++ irc.rb
@@ -2,22 +2,28 @@
 
 require 'socket'
 
+#puts with appended timestamp
+def tsputs (string)
+  puts "#{Time.now.strftime("%F %T")} #{string}"
+end
+  
+
 #join channels
 def join (socket, channels)
   for channel in channels
-    puts "SEND: JOIN ##{channel}"
+    tsputs "SEND: JOIN ##{channel}"
     socket.puts "JOIN ##{channel}"
   end
 end
 
 def identify (socket, passwd)
-  puts "SEND: PRIVMSG NickServ :IDENTIFY #{passwd}"
+  tsputs "SEND: PRIVMSG NickServ :IDENTIFY #{passwd}"
   socket.puts "PRIVMSG NickServ :IDENTIFY #{passwd}"
 end
 
 #pong server
 def pong (socket, server)
-  puts "SEND: PONG #{server}"
+  tsputs "SEND: PONG #{server}"
   socket.puts "PONG #{server}"
 end
 
@@ -25,14 +31,14 @@ channels = ["minecraft"]
 
 irc = TCPSocket.new 'irc.tamu.edu', 6667
 
-puts "SEND: NICK silvrfish"
+tsputs "SEND: NICK silvrfish"
 irc.puts "NICK silvrfish"
-puts "SEND: USER silvrfish * * :silvrfish"
+tsputs "SEND: USER silvrfish * * :silvrfish"
 irc.puts "USER silvrfish * * :silvrfish"
 
 #wait for auth to finish, join channels and identify upon server sending 001
 while line = irc.gets
-  puts line
+  tsputs line
   if line.include? "001"
     identify irc, "ichaizeu"
     join irc, channels
@@ -43,7 +49,7 @@ end
 #puts output
 out = Thread.new{
   while line = irc.gets
-    puts line
+    tsputs line
     #listen for a respond to pings
     if line.include? "PING :"
       server = line.split(':')[1]
@@ -55,14 +61,14 @@ out = Thread.new{
       index = line.index('&') + 1
       command = line[index..line.length].split(' ')[0]
       if command == "botsnack"
-        puts "SEND: PRIVMSG #minecraft :*rrrgghh*"
+        tsputs "SEND: PRIVMSG #minecraft :*rrrgghh*"
         irc.puts "PRIVMSG #minecraft :*rrrgghh*"
       end
       if command == "players"
         players = %x[/home/ross/bin/mcrcon -H 127.0.0.1 -p rossroll1234 -P 20155 list]
         players.sub!(":", ": ")
         players.rstrip!
-        puts "SEND: PRIVMSG #minecraft :#{players}"
+        tsputs "SEND: PRIVMSG #minecraft :#{players}"
         irc.puts "PRIVMSG #minecraft :#{players}"
       end
     end
@@ -71,7 +77,7 @@ out = Thread.new{
 
 console_in = Thread.new{
   while input = gets
-    puts "SEND: PRIVMSG #minecraft :#{input}"
+    tsputs "SEND: PRIVMSG #minecraft :#{input}"
     irc.puts "PRIVMSG #minecraft :#{input}"
   end
 }
