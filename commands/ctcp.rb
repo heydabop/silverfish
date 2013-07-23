78fb7633a8b9ee31562e64dd9580460a5cf11706
heydabop <heydabop@gmail.com>
2013-07-23 14:57:07 -0500

    Actually added CTCP support from 9bd576d54f... >.>

---
+++ commands/ctcp.rb
@@ -0,0 +1,24 @@
+def Commands.ctcp(socket, nick, channel, args) #args[0] is CTCP command
+  command = args[0]
+  case command
+  when "CLIENTINFO"
+    tsputs "SEND: NOTICE #{nick} :\u0001CLIENTINFO VERSION SOURCE USERINFO CLIENTINFO TIME FINGER\u0001"
+    socket.puts "NOTICE #{nick} :\u0001CLIENTINFO VERSION SOURCE USERINFO CLIENTINFO TIME FINGER\u0001"
+  when "VERSION"
+    tsputs "SEND: NOTICE #{nick} :\u0001VERSION Heydabot Co. Silverfish Edition vALPHA -- #{`ruby --version`}\u0001"
+    socket.puts "NOTICE #{nick} :\u0001VERSION Heydabot Co. Silverfish Edition vALPHA -- #{`ruby --version`}\u0001"
+  when "SOURCE"
+    tsputs "SEND: NOTICE #{nick} :\u0001SOURCE https://github.com/heydabop/silverfish\u0001"
+    socket.puts "NOTICE #{nick} :\u0001SOURCE https://github.com/heydabop/silverfish\u0001"
+  when "USERINFO"
+    tsputs "SEND: NOTICE #{nick} :\u0001USERINFO silverfish\u0001"
+    socket.puts "NOTICE #{nick} :\u0001USERINFO silverfish\u0001"
+  when "TIME"
+    tsputs "SEND: NOTICE #{nick} :\u0001TIME #{`date`}"
+    socket.puts "NOTICE #{nick} :\u0001TIME #{`date`}"
+  when "FINGER"
+    tsputs "SEND: NOTICE #{nick} :\u0001FINGER silverfish (silvrfish)  Idle NEVER"
+    socket.puts "NOTICE #{nick} :\u0001FINGER silverfish (silvrfish)  Idle NEVER"
+  end
+end
+    
