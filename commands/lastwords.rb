c12d55d68a0a73043a8bf3b960adf72a99c6a40b
heydabop <heydabop@gmail.com>
2013-07-25 15:17:48 -0500

    Added randomization to lastwords

--- commands/lastwords.rb
+++ commands/lastwords.rb
@@ -1,6 +1,29 @@
 def Commands.lastwords(socket, nick, channel, args)
   require 'set'
   require 'time'
+
+=begin
+  class DeathInfo
+    def initialize(death, word, ti)
+      @death_message = death
+      @last_word = word
+      @time = ti
+    end
+    def death_message
+      return @death_message
+    end
+    def last_word
+      return @last_word
+    end
+    def time
+      return @time
+    end
+  end
+=end
+
+  deathInfo = Struct.new(:last_word, :time, :death_message)
+  deaths = Array.new
+
   user = args[0].downcase
   death_types = Set.new ["#{user} was squashed by a falling anvil", "#{user} was pricked to death", "#{user} walked into a cactus whilst trying to escape", "#{user} was shot by arrow", "#{user} drowned", "#{user} blew up", "#{user} was blown up by", "#{user} hit the ground too hard", "#{user} fell off a ladder", "#{user} fell off some vines", "#{user} fell out of the water", "#{user} fell from a high place", "#{user} fell into a patch of fire", "#{user} fell into a patch of cacti", "#{user} was doomed to fall", "#{user} was shot off some vines by", "#{user} was shot off a ladder by", "#{user} was blown from a high place by", "#{user} went up in flames", "#{user} burned to death", "#{user} was burnt to a crisp whilst fighting", "#{user} walked into a fire whilst fighting", "#{user} was slain by", "#{user} was shot by", "#{user} was fireballed by", "#{user} was killed by", "#{user} got finished off by", "#{user} was slain by", "#{user} tried to swim in lava", "#{user} tried to swim in lava while trying to escape", "#{user} died", "#{user} got finished off by", "#{user} was slain by", "#{user} was shot by", "#{user} was killed by", "#{user} was killed by magic", "#{user} starved to death", "#{user} suffocated in a wall", "#{user} was killed while trying to hurt", "#{user} was pummeled by", "#{user} fell out of the world", "#{user} fell from a high place and fell out of the world", "#{user} was knocked into the void by", "#{user} withered away", "#{user} was sucked to the moon"]
   lines = Array.new
@@ -55,16 +78,17 @@ def Commands.lastwords(socket, nick, channel, args)
                 end
               end
               
-              tsputs "SEND: PRIVMSG #{channel} :#{last_word} | About #{time} later, #{death_message}"
-              socket.puts "PRIVMSG #{channel} :#{last_word} | About #{time} later, #{death_message}"
-              return
+              deaths.push(deathInfo.new(last_word, time, death_message))
             end
           }
-          return
         end
       }
     end
   }
+
+  death = deaths.sample
+  tsputs "SEND: PRIVMSG #{channel} :#{death.last_word} | About #{death.time} later, #{death.death_message}"
+  socket.puts "PRIVMSG #{channel} :#{death.last_word} | About #{death.time} later, #{death.death_message}"
 end
               
 
