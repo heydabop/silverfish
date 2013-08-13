2f93c772a0ada4ff65b03c4c1fa2c5ce1b731601
heydabop <heydabop@gmail.com>
2013-08-12 23:05:17 -0500

    Fixed death notify

--- mc_irc.rb
+++ mc_irc.rb
@@ -1,5 +1,5 @@
 def mc_irc irc_socket
-  death_types = Set.new ["(\w{,16}) was", "(\w{,16}) walked into a cactus whilst trying to escape", "(\w{,16}) drowned", "(\w{,16}) blew up", "(\w{,16}) hit the ground too hard", "(\w{,16}) fell", "(\w{,16}) went up in flames", "(\w{,16}) burned to death","(\w{,16}) walked into a fire whilst fighting", "(\w{,16}) got finished off by", "(\w{,16}) tried to swim in lava", "(\w{,16}) died", "(\w{,16}) starved to death", "(\w{,16}) suffocated in a wall", "(\w{,16}) withered away"]  
+  death_types = Set.new ['\w{,16} was', '\w{,16} walked into a cactus whilst trying to escape', '\w{,16} drowned', '\w{,16} blew up', '\w{,16} hit the ground too hard', '\w{,16} fell', '\w{,16} went up in flames', '\w{,16} burned to death','\w{,16} walked into a fire whilst fighting', '\w{,16} got finished off by', '\w{,16} tried to swim in lava', '\w{,16} died', '\w{,16} starved to death', '\w{,16} suffocated in a wall', '\w{,16} withered away']  
 
   tail_process = IO.popen "tail -n 0 -f #{SERVER_LOG}"
   $tail_pid = tail_process.pid #init in main.rb, used in spawner.rb
@@ -34,18 +34,18 @@ def mc_irc irc_socket
         puts e.backtrace.inspect
       end
     else
-      message = MatchData
+      puts death_types.size
       death_types.each {|death|
         if (message = %r{^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} \[INFO\] (#{death}.*)}.match(log_line)) != nil
-          message = message[1].strip
-          tsputs "SEND: NOTICE #minecraft :#{message}"
-          begin
-            irc_socket.puts "NOTICE #minecraft :#{message}"
-          rescue IOError => e
-            puts e.message
-            puts e.backtrace.inspect
-          end
-          break
+         message = message[1].strip
+         tsputs "SEND: NOTICE #minecraft :#{message}"
+         begin
+           irc_socket.puts "NOTICE #minecraft :#{message}"
+         rescue IOError => e
+           puts e.message
+           puts e.backtrace.inspect
+         end
+         break
         end
       }
     end
