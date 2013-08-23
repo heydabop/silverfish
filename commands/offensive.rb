da0191e34d679cb3ed3d42dc32ac295dc0b350e0
heydabop <heydabop@gmail.com>
2013-08-23 18:47:45 -0500

    Replaced every other newline in offensive with /

--- commands/offensive.rb
+++ commands/offensive.rb
@@ -1,5 +1,5 @@
 def Commands.offensive(socket, nick, channel, args)
-  fortune = `fortune -os`.gsub(/\t/, '  ').split("\n")
+  fortune = `fortune -os`.gsub(/\t/, '  ').gsub(/(.*)\n(.*\n)/, '\1 / \2').rstrip.split("\n")
   fortune.each {|line|
     tsputs "SEND: PRIVMSG #{channel} :#{line}"
     socket.puts "PRIVMSG #{channel} :#{line}"
