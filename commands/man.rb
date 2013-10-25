17e51cdecf585cd9636f037aea26185b3cfb8d3a
heydabop <heydabop@gmail.com>
2013-10-25 17:40:48 -0500

    add man command

---
+++ commands/man.rb
@@ -0,0 +1,11 @@
+def Commands.man(socket, nick, channel, args)
+  com = args[0]
+  manout = `man #{com}`
+  if $?.exitstatus == 16
+        tsputs "SEND: PRIVMSG #{channel} :No manual entry for #{com}"
+        socket.puts "PRIVMSG #{channel} :No manual entry for #{com}"
+  elsif((firstline = %r{(#{com}.*)\n}.match(manout)) != nil)
+    tsputs "SEND: PRIVMSG #{channel} :#{firstline[1]}"
+    socket.puts "PRIVMSG #{channel} :#{firstline[1]}"
+  end
+end
