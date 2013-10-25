5b6c7ecc1d08d0439877439fb082d77813288bf0
heydabop <heydabop@gmail.com>
2013-10-25 17:46:54 -0500

    case insensitive, dont match headers

--- commands/man.rb
+++ commands/man.rb
@@ -4,7 +4,7 @@ def Commands.man(socket, nick, channel, args)
   if $?.exitstatus == 16
         tsputs "SEND: PRIVMSG #{channel} :No manual entry for #{com}"
         socket.puts "PRIVMSG #{channel} :No manual entry for #{com}"
-  elsif((firstline = %r{(#{com}.*)\n}.match(manout)) != nil)
+  elsif((firstline = %r{(#{com} - .*)\n}i.match(manout)) != nil)
     tsputs "SEND: PRIVMSG #{channel} :#{firstline[1]}"
     socket.puts "PRIVMSG #{channel} :#{firstline[1]}"
   end
