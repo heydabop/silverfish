9490071f74f163b3f9633bc9924df90312f215db
heydabop <heydabop@gmail.com>
2014-01-22 18:59:44 -0600

    added LTC/USD command

---
+++ commands/ltc.rb
@@ -0,0 +1,7 @@
+def Commands.ltc(socket, nick, channel, args)
+  json = URI.parse('https://btc-e.com/api/2/ltc_usd/ticker').read
+  ticker = JSON.parse(json)
+  message = %Q(LTC/USD 24hr HI: #{ticker["ticker"]["high"]} - 24hr LOW: #{ticker["ticker"]["low"]} - Buy: #{ticker["ticker"]["buy"]} - Sell: #{ticker["ticker"]["sell"]})
+  tsputs "SEND: PRIVMSG #{channel} :#{message}"
+  socket.puts "PRIVMSG #{channel} :#{message}"
+end
