42bd78e169335b8c7719148b7b2e5ba83e9f34c1
heydabop <heydabop@gmail.com>
2014-02-26 14:34:33 -0600

    excuse now uses nokogiri

--- commands/excuse.rb
+++ commands/excuse.rb
@@ -1,15 +1,6 @@
 def Commands.excuse(socket, nick, channel, args)
-  html = Net::HTTP.get('programmingexcuses.com', '/')
-  lines = html.split "\n"
-  message = ""
-  lines.each {|line|
-    if line.include? %Q(href="/")
-      endIndex = line.rindex %Q(</a>)
-      index = (line.rindex %Q(;">), endIndex) + 3
-      message = line[index...endIndex]
-      break
-    end
-  }
+  page = Nokogiri::HTML(open("http://programmingexcuses.com/"))
+  message = page.css('a').text.rstrip
   tsputs "SEND: PRIVMSG #{channel} :#{message}."
   socket.puts "PRIVMSG #{channel} :#{message}."
 end
